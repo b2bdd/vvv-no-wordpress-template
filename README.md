16cc1080cb317edac5a803b9254e5a05a73f052a
Doug Mitchell <dmitchell@b2bdd.com>
2018-03-22 14:03:45 -0400

    Created script based on original VVV template

---
+++ README.md
@@ -0,0 +1,4 @@
+# VVV Site Template Without WordPress
+
+## Overview
+This template creates a site in WordPress without any database or files, except for index.html
---
+++ provision/vvv-init.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Provision WordPress Stable
+
+DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
+DOMAINS=`get_hosts "${DOMAIN}"`
+
+# Nginx Logs
+mkdir -p ${VVV_PATH_TO_SITE}/log
+touch ${VVV_PATH_TO_SITE}/log/error.log
+touch ${VVV_PATH_TO_SITE}/log/access.log
+
+# Create site root directory and add index.html
+if [[ ! -d "${VVV_PATH_TO_SITE}/htdocs" ]]; then
+    echo "Creating Site Root Directory..."
+    mkdir -p ${VVV_PATH_TO_SITE}/htdocs
+    cp -f "${VVV_PATH_TO_SITE}/provision/index.html" "${VVV_PATH_TO_SITE}/htdocs/index.html"
+    sed -i "s/{{SITENAME}}/${VVV_SITE_NAME}/" "${VVV_PATH_TO_SITE}/htdocs/index.html"
+fi
+
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
+    sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
